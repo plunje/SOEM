e41a292667def26164fb665c24dc0c208953f6e7
plunje <63554440+plunje@users.noreply.github.com>
2024-05-12 18:12:34 +0200

    Update werkplaats2.py

--- test/linux/werkplaats2/werkplaats2.py
+++ test/linux/werkplaats2/werkplaats2.py
@@ -23,48 +23,47 @@ def main():
     master.open(netif)
 
     try:
-        if not master.config_init() or master.slave_count == 0:
+        if not master.config_init() or len(master.slaves) == 0:
             print(f'No slaves found or configuration failed on {netif}.')
             master.close()
             return 1
 
-        print(f'{master.slave_count} slaves found and configured.')
+        print(f'{len(master.slaves)} slaves found and configured.')
 
         master.config_map()
         master.config_dc()
 
         print('Slaves mapped and configured for DC.')
 
-        master.state = pysoem.SAFEOP_STATE
-        master.write_state()
-
-        while master.state_check(pysoem.SAFEOP_STATE, 5000) != pysoem.SAFEOP_STATE:
-            print('Slaves not in safe operational state.')
-            time.sleep(1)
 
         master.state = pysoem.OP_STATE
         master.write_state()
+        
+        while master.state_check(pysoem.OP_STATE, 50000) != pysoem.OP_STATE:
+            print('Slaves not in operational state.')
+            time.sleep(1)
+            
+        print('Slaves in operational state.')
 
-        if master.state_check(pysoem.OP_STATE, 5000) == pysoem.OP_STATE:
+        if master.state_check(pysoem.OP_STATE, 50000) == pysoem.OP_STATE:
             print('Operational state reached for all slaves.')
             while True:
                 master.send_processdata()
                 master.receive_processdata(10000)
 
-                if slave_index > master.slave_count:
-                    print(f'Slave index out of range. Only {master.slave_count} slaves available.')
+                if slave_index > len(master.slaves):
+                    print(f'Slave index out of range. Only {len(master.slaves)} slaves available.')
                     break
 
-                inputs = master.slaves[slave_index - 1].inputs  # -1 because Python uses 0-based indexing
-
-                if inputs:
+                slave = master.slaves[slave_index - 1]
+                if slave.input:
                     # Extract the specified bit to check the sensor state
-                    input_state = bool(inputs[0] & (1 << (channel_index - 1)))
+                    input_state = bool(slave.input[0] & (1 << (channel_index - 1)))
                     print(f'Input: {"true" if input_state else "false"}')
 
                 time.sleep(1)  # Wait 1 second
         else:
-            print('Failed to reach operational state.')
+            print(f'Failed to reach operational state. {master.state_check(pysoem.OP_STATE, 50000)}')
 
         master.close()
         return 0
